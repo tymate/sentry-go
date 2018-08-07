028dc981cc40ea071162ceddb69db425e1d0a737
Paul  d'Hubert <paul@tymate.com>
2018-08-07 10:28:35 +0200

    feat: hack GAE support

---
+++ http_gae_transport.go
@@ -0,0 +1,53 @@
+package sentry
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"google.golang.org/appengine/urlfetch"
+)
+
+const (
+	gaeContextClass = "sentry-go.gae-context"
+)
+
+type gaeTransport struct {
+	*httpTransport
+}
+
+func NewGAETransport(ctx context.Context) Transport {
+	t := newHTTPTransport()
+	httpT := t.(*httpTransport)
+	return &gaeTransport{httpT}
+}
+
+func (t *gaeTransport) Send(dsn string, p Packet) error {
+	ctxOpt, ok := p.(packet)[gaeContextClass]
+	if !ok {
+		return errors.New("missing Google AppEngine context")
+	}
+
+	opt, ok := ctxOpt.(*gaeContextOption)
+	if !ok {
+		return errors.New("invalid GAEContext option")
+	}
+
+	t.client = urlfetch.Client(opt.ctx)
+	return t.Send(dsn, p)
+}
+
+type gaeContextOption struct {
+	ctx context.Context
+}
+
+func (o *gaeContextOption) Class() string {
+	return gaeContextClass
+}
+
+func (o *gaeContextOption) Omit() bool {
+	return true
+}
+
+func GAEContext(ctx context.Context) Option {
+	return &gaeContextOption{ctx}
+}
